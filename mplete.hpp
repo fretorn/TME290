df3815deb1a5c7d8db8f0313844d470e55fd4d17
fretorn <fretorn@student.chalmers.se>
2018-03-24 17:46:32 +0100

    Lecture2Slide52

---
+++ mplete.hpp
@@ -0,0 +1,14 @@
+--2018-03-24 13:23:39--  http://https/
+Resolving https (https)... failed: Name or service not known.
+wget: unable to resolve host address ‘https’
+://: Scheme missing.
+--2018-03-24 13:23:39--  http://chrberger.github.io/libcluon/headeronly/cluon
+Resolving chrberger.github.io (chrberger.github.io)... 151.101.85.147, 2a04:4e42:14::403
+Connecting to chrberger.github.io (chrberger.github.io)|151.101.85.147|:80... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://chrberger.github.io/libcluon/headeronly/cluon [following]
+--2018-03-24 13:23:39--  https://chrberger.github.io/libcluon/headeronly/cluon
+Connecting to chrberger.github.io (chrberger.github.io)|151.101.85.147|:443... connected.
+HTTP request sent, awaiting response... 404 Not Found
+2018-03-24 13:23:39 ERROR 404: Not Found.
+
